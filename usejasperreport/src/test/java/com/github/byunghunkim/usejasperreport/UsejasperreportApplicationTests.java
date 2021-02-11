ccad888bb8100361d7650507cfb59e3fd6efe671
Byunghun Kim <flamefeat@gmail.com>
2021-02-11 11:15:51 +0900

    add test

---
+++ usejasperreport/src/test/java/com/github/byunghunkim/usejasperreport/UsejasperreportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.byunghunkim.usejasperreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsejasperreportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
