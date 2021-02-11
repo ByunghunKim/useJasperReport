2238625d04b9e3dcf3b0acd97370227bb02f849b
Byunghun Kim <flamefeat@gmail.com>
2021-02-11 11:33:11 +0900

    init springboot

---
+++ usejasperreport/src/main/java/com/github/byunghunkim/usejasperreport/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.github.byunghunkim.usejasperreport;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(UsejasperreportApplication.class);
+	}
+
+}
